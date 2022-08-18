1a3c0c9bc70e46e873381293745c349e7d905577
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-18 08:49:41 -0300

    Criado requisição simples com fetch no useEffect para consumo da API do json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css'
 
+import { useEffect, useState } from 'react'
+
+const url = 'http://localhost:3000/products'
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  // 1 Resgatando dados
+  useEffect(() => {
+
+    async function fecthData() {
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    fecthData()
+  }, [])
+
+  //console.log(products)
+
   return (
     <div className="App">
-
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map(product => (<li key={product.id}>{product.name} - {product.price}</li>))}
+      </ul>
     </div>
   )
 }
