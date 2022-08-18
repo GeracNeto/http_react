8abe98a59d400ae82da28208998c307338d66924
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-18 09:43:00 -0300

    Criado consts name e price, criado função handleSubmit para fazer um POST no json-server e salvar os dados no arquivo db.json

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [products, setProducts] = useState([])
 
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
   // 1 Resgatando dados
   useEffect(() => {
 
@@ -23,12 +26,54 @@ function App() {
 
   //console.log(products)
 
+  // Add de produtos
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+      /*
+      Mesmo que dizer:
+
+      name: name
+      price: price
+
+      Isso porque as chaves possuem os mesmos nomes que os valores
+      */
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+    console.log(product)
+  }
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map(product => (<li key={product.id}>{product.name} - {product.price}</li>))}
+        {products.map(product => (<li key={product.id}>{product.name} - R$ {product.price}</li>))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={e => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name='name' onChange={e => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value='Criar' />
+        </form>
+      </div>
     </div>
   )
 }
